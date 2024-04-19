f677e61da3b9f8dd1ebe83e3ffe58c09fa7737a3
ADMIN\Administrator <tien.nguyen080816qsb@hcmut.edu.vn>
2024-04-19 15:37:39 +0700

    update files

--- Lab3/uart.py
+++ Lab3/uart.py
@@ -3,6 +3,10 @@ import serial.tools.list_ports
 # import time
 # import  sys
 # from  Adafruit_IO import  MQTTClient
+# real, com3 for hercules, com4 for python
+# virtual, com2 for hercules, com3 for python
+# echo "!1:T:60.38##!2:H:71.88##!3:L:300.14##" > /dev/pts/2, 2 can be another number when socat in wsl again be careful
+# echo "!1:T:35.23##!2:H:60.27##!3:L:200.96##" > /dev/pts/2
 
 def getPort():
     ports = serial.tools.list_ports.comports()
@@ -15,7 +19,8 @@ def getPort():
             splitPort = strPort.split(" ")
             commPort = (splitPort[0])
     # return commPort
-    return "COM4"
+    # return "COM4" # directly return
+    return '/dev/pts/5'
 
 if getPort() != "None": # check if getPort is successful
     ser = serial.Serial(port=getPort(), baudrate=115200)
@@ -50,7 +55,7 @@ def readSerial(client):
                 mess = mess[end+1:]
 
 def writeData(data):
-    ser.write(str(data).encode())
+    ser.write(str(data).encode("utf-8"))
 # while True:
 #     readSerial()
 #     time.sleep(1)
