cff42e1e389fff274491de107e33e78178978253
ADMIN\Administrator <tien.nguyen080816qsb@hcmut.edu.vn>
2024-04-21 16:39:16 +0700

    justify the symbol of sensors for more clear and add button_1 and button_2 to messageArived() func to synchronized the data wwhen press button

--- Lab4/app/src/main/java/com/example/firstandroidapp/MainActivity.java
+++ Lab4/app/src/main/java/com/example/firstandroidapp/MainActivity.java
@@ -6,14 +6,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.github.angads25.toggle.interfaces.OnToggledListener;
+import com.github.angads25.toggle.model.ToggleableView;
+import com.github.angads25.toggle.widget.LabeledSwitch;
+
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.nio.charset.Charset;
 
 public class MainActivity extends AppCompatActivity
 {
     MQTTHelper mqttHelper;
     TextView txtTemp, txtHumid;
+    LabeledSwitch buttonLed1, buttonLed2;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -22,9 +30,59 @@ public class MainActivity extends AppCompatActivity
 
         txtTemp = findViewById(R.id.txtTemperature);
         txtHumid = findViewById(R.id.txtHumidity);
+        buttonLed1 = findViewById(R.id.buttonLed1);
+        buttonLed2 = findViewById(R.id.buttonLed2);
+
+        // from mobile to adafruit
+        buttonLed1.setOnToggledListener(new OnToggledListener() {
+            @Override
+            public void onSwitched(ToggleableView toggleableView, boolean isOn)
+            {
+                if(isOn == true)
+                {
+                    sendDataMQTT("Nhat_Tien_2002/feeds/button-1", "1");
+                }
+                else
+                {
+                    sendDataMQTT("Nhat_Tien_2002/feeds/button-1", "0");
+                }
+            }
+        });
+
+        buttonLed2.setOnToggledListener(new OnToggledListener() {
+            @Override
+            public void onSwitched(ToggleableView toggleableView, boolean isOn)
+            {
+                if(isOn == true)
+                {
+                    sendDataMQTT("Nhat_Tien_2002/feeds/button-2", "1");
+                }
+                else
+                {
+                    sendDataMQTT("Nhat_Tien_2002/feeds/button-2", "0");
+                }
+            }
+        });
 
         startMQTT();
     }
+
+    public void sendDataMQTT(String topic, String value)
+    {
+        MqttMessage msg = new MqttMessage();
+        msg.setId(1234);
+        msg.setQos(0);
+        msg.setRetained(false);
+
+        byte[] b = value.getBytes(Charset.forName("UTF-8"));
+        msg.setPayload(b);
+
+        try {
+            mqttHelper.mqttAndroidClient.publish(topic, msg);
+        }catch (MqttException e){
+        }
+    }
+
     public void startMQTT()
     {
         mqttHelper = new MQTTHelper(this);
@@ -39,6 +97,7 @@ public class MainActivity extends AppCompatActivity
 
             }
 
+            // from adafruit to mobile
             @Override
             public void messageArrived(String topic, MqttMessage message) throws Exception {
                 Log.d("TEST", topic + "***" + message.toString());
@@ -49,6 +108,28 @@ public class MainActivity extends AppCompatActivity
                 else if(topic.contains("sensor-2"))
                 {
                     txtHumid.setText(message.toString() + "%");
+                }
+                else if(topic.contains("button-1"))
+                {
+                    if(message.toString().equals("1"))
+                    {
+                        buttonLed1.setOn(true);
+                    }
+                    else
+                    {
+                        buttonLed1.setOn(false);
+                    }
+                }
+                else if(topic.contains("button-2"))
+                {
+                    if(message.toString().equals("1"))
+                    {
+                        buttonLed2.setOn(true);
+                    }
+                    esle
+                    {
+                        buttonLed2(false);
+                    }
                 }
             }
 
